f2e673f4ba02e80d8644266dd47ce6297e4b39a8
matthewmillerh <matthewmillerh@outlook.com>
2024-09-23 17:27:21 +0200

    netlify fix test

--- src/views/AvailableWorksVue.vue
+++ src/views/AvailableWorksVue.vue
@@ -1,18 +1,18 @@
 <script setup>
-const images = import.meta.glob('@/assets/images/*.jpg')
+const images = import.meta.glob('/public/images/*.jpg')
 
-console.log(images)
 </script>
 
 <template>
     <div class="flex justify-center flex-wrap md:ml-60 md:mr-60 lg:ml-80 lg:mr-80" v-viewer.static="{transition: false, scalable: false, rotatable: false, fullscreen: false}">
         <div v-for="image in images" :key="image">
             <div class="">
-                <img :src="'.' + image.name" alt="Emily" class="h-40 w-32 p-1 m-3 border border-gray-300 bg-gray-100 rounded">
-                <img src="../assets/images/raesmith.jpg" alt="">
+                <img :src="image.name" alt="Emily" class="h-40 w-32 p-1 m-3 border border-gray-300 bg-gray-100 rounded">
+                
             </div>      
         </div>
     </div>
+    <img src="/public/images/raesmith.jpg" alt="">
 </template>
 
 <style>
