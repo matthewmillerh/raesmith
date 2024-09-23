27c165516758acb4fa32f8a88d3270e188a605ac
matthewmillerh <matthewmillerh@outlook.com>
2024-09-23 17:30:42 +0200

    netlify fix test

--- src/views/AvailableWorksVue.vue
+++ src/views/AvailableWorksVue.vue
@@ -1,5 +1,5 @@
 <script setup>
-const images = import.meta.glob('/public/images/*.jpg')
+const images = import.meta.glob('@/assets/images/*.jpg')
 
 </script>
 
@@ -7,7 +7,7 @@ const images = import.meta.glob('/public/images/*.jpg')
     <div class="flex justify-center flex-wrap md:ml-60 md:mr-60 lg:ml-80 lg:mr-80" v-viewer.static="{transition: false, scalable: false, rotatable: false, fullscreen: false}">
         <div v-for="image in images" :key="image">
             <div class="">
-                <img :src="image.name" alt="Emily" class="h-40 w-32 p-1 m-3 border border-gray-300 bg-gray-100 rounded">
+                <img :src="image.name.slice(4)" alt="Emily" class="h-40 w-32 p-1 m-3 border border-gray-300 bg-gray-100 rounded">
                 
             </div>      
         </div>
