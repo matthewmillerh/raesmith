7a73c0ec60c901b6c034dfc418dfcf684cc3802c
matthewmillerh <matthewmillerh@outlook.com>
2024-09-23 15:55:30 +0200

    sync commit

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,23 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+      <div class="w-full min-h-24 p-1 border-b border-black mb-4">
+        <h3 class="font-semibold text-2xl text-center mt-2">Emily Rae Smith Labuschagne</h3>
+        <nav class="text-center mt-3 text-base">
+          <RouterLink class="ml-2 mr-2 hover:underline" to="/">Home</RouterLink>
+          <RouterLink class="ml-2 mr-2 hover:underline" to="/available-works">Available Works</RouterLink>
+          <RouterLink class="ml-2 mr-2 hover:underline" to="/about">About</RouterLink>
+          <RouterLink class="ml-2 mr-2 hover:underline" to="/contact">Contact</RouterLink>
+        </nav>
+      </div>     
+    </div> <!-- End Wrapper -->
   </header>
 
   <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped>
+.router-link-active{
+  text-decoration: underline;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -6,11 +6,16 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+//imports for image viewer
+import 'viewerjs/dist/viewer.css'
+import VueViewer from 'v-viewer'
+
 import './index.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueViewer)
 
 app.mount('#app')
